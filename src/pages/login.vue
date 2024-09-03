707feb5e411b55f307d351257001c78536f2394e
hanjituan <1048416203@qq.com>
2024-09-04 00:44:16 +0800

    add background image

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,17 +1,23 @@
 <template>
 
-	<div
-		class="p-0 bg-[#f3f3f3] w-full h-full flex justify-center items-center bg-gradient-to-br from-blue-500 to-green-500">
+	<!--渐变背景 bg-gradient-to-br from-blue-500 to-green-500 -->
+	<div class="p-0 bg-[#f3f3f3] w-full h-full flex justify-center items-center relative login-page z-index-1">
+
+		<!-- <img src="@/assets/images/login-bg.jpg" alt=""> -->
 
 		<el-form ref="loginFormRef" :model="loginForm" label-width="150" :rules="rules" size="large"
-			label-position="top" class="w-500px h-300px bg-[#ffffffb6] text-[#262D30] p-8 shadow-md rounded-md ">
+			label-position="top" class="w-500px bg-[#ffffffb6] text-[#262D30] p-8 shadow-md rounded-md ">
+
+			<div class="py-4 font-bold text-xl text-center text-[#262D30] mb-4">
+				我 element-plus + vue3 + ts + vite
+			</div>
 
 			<el-form-item label="用户名" prop="userName">
-				<el-input v-model="loginForm.userName" class="roll-form-item" placeholder="请输入用户名" clearable />
+				<el-input v-model="loginForm.userName" class="login-form-item" placeholder="请输入用户名" clearable />
 			</el-form-item>
 
 			<el-form-item label="密码" prop="password">
-				<el-input v-model="loginForm.password" class="roll-form-item" placeholder="请输入密码" type="password" />
+				<el-input v-model="loginForm.password" class="login-form-item" placeholder="请输入密码" type="password" />
 			</el-form-item>
 
 			<el-form-item>
@@ -19,6 +25,10 @@
 				<el-button @click='reset'>重置</el-button>
 			</el-form-item>
 		</el-form>
+
+		<div class="absolute w-400px right-10px -bottom-200px -z-index-1">
+			<img class="w-full" src="@/assets/images/after.png" alt="">
+		</div>
 	</div>
 
 </template>
@@ -77,11 +87,18 @@ const login = async () => {
 </script>
 
 <style scoped>
-.roll-form-item {
+.login-page {
+	background-image: url('@/assets/images/login-bg.jpg');
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+}
+
+.login-form-item {
 	padding: 0;
 }
 
-.roll-form-item>>>.el-input__inner:focus {
+.login-form-item>>>.el-input__inner:focus {
 	--tw-ring-color: transparent
 }
 </style>
